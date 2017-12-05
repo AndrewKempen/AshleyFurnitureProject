beb0ffcd9fbd18fc4f0af48e2423b5da36123fd8
vosnet <connor.farnworth@gmail.com>
2017-12-05 09:08:14 -0600

    Updated

---
+++ main.js
@@ -0,0 +1,6 @@
+function sec1 (){
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 10;
+} ;
+// todo; get function to change time ?
+// todo v2 
