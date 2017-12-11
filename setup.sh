9ce50f0e0238c305dc38780c20f6127707b28768
Andrew Kempen <Wishbonea@users.noreply.github.com>
2017-12-11 08:01:02 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Set locale
+sudo echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+
+#Install required packages
+sudo apt-get purge wolfram-engine -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install git build-essential cmake vim
+
+#Download and compile CORE Vision code
+cd /home/pi
+git clone https://github.com/Wishbonea/AshleyFurnitureProject/
+cd AshleyFurnitureProject
+sudo chmod +x update.sh
+sudo cp AshleyFurnitureProject.service /lib/systemd/system/AshleyFurnitureProject.service
+sudo systemctl daemon-reload
+sudo systemctl enable AshleyFurnitureProject.service
