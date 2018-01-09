b1ffa3718bed6d2b5ec4c347d7ac6768faa4ba4f
Connor Farnworth <connor.farnworth@gmail.com>
2018-01-09 14:46:49 -0600

    Added scripting, better div support & some CSS

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ function sec1() {
   console.log(document.getElementById("ev1").currentTime);
   document.getElementById("ev1").currentTime = 10;
 };
+
+function sec2() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 20;
+};
+function sec3() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 30;
+};
+function sec4() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 40;
+};
 // todo; get function to change time ?
 // todo v2
 // todo; get times and names for sections
