41eb6f0eb847564ca20bdee800cb5d6f6e4840fc
Connor Farnworth <connor.farnworth@gmail.com>
2018-01-09 08:54:55 -0600

    formatting, jpg to png

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function sec1 (){
+function sec1() {
   console.log(document.getElementById("ev1").currentTime);
   document.getElementById("ev1").currentTime = 10;
-} ;
+};
 // todo; get function to change time ?
 // todo v2
 // todo; get times and names for sections
