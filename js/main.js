cedd328f358db5be1256b33870ba10e2188c2ebc
Connor Farnworth <connor.farnworth@gmail.com>
2018-01-17 09:45:08 -0600

    Added timings and buttons

--- js/main.js
+++ js/main.js
@@ -1,19 +1,56 @@
 function sec1() {
   console.log(document.getElementById("ev1").currentTime);
-  document.getElementById("ev1").currentTime = 10;
+  document.getElementById("ev1").currentTime = 5;
 };
 
 function sec2() {
   console.log(document.getElementById("ev1").currentTime);
-  document.getElementById("ev1").currentTime = 20;
+  document.getElementById("ev1").currentTime = 9;
 };
+
 function sec3() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 14;
+};
+
+function sec4() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 19;
+};
+
+function sec5() {
   console.log(document.getElementById("ev1").currentTime);
   document.getElementById("ev1").currentTime = 30;
 };
-function sec4() {
+
+function sec6() {
   console.log(document.getElementById("ev1").currentTime);
-  document.getElementById("ev1").currentTime = 40;
+  document.getElementById("ev1").currentTime = 43;
+};
+
+function sec7() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 60;
+};
+
+function sec8() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 71;
+};
+
+function sec9() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 89;
+};
+
+function sec10() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 96;
+};
+
+function sec11() {
+  console.log(document.getElementById("ev1").currentTime);
+  document.getElementById("ev1").currentTime = 106;
 };
 // todo; get function to change time ?
 // todo v2
